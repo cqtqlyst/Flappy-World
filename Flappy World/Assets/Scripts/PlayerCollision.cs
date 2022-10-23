110fb83e7889ac76f6e02ac5d592767419336cd3
cqtqlyst <aayush.gaywala@gmail.com>
2022-10-23 12:56:22 -0700

    Struggling with Player Collision

---
+++ Flappy World/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+
+    private void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Platform")
+        {
+            Debug.Log("We have hit a platform!");
+
+            movement.enabled = false;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
